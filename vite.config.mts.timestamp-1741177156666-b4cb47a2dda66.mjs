04367e00022d44e339c32c0c85a527e187e4076c
bananvyhe <loadonden@yahoo.com>
2025-03-05 20:01:18 +0500

    nacked project

---
+++ vite.config.mts.timestamp-1741177156666-b4cb47a2dda66.mjs
@@ -0,0 +1,14 @@
+// vite.config.mts
+import { defineConfig } from "file:///home/deploy/pixelnew/.yarn/__virtual__/vite-virtual-f6b8394bcd/2/.yarn/berry/cache/vite-npm-5.4.14-4903607bfe-10c0.zip/node_modules/vite/dist/node/index.js";
+import RubyPlugin from "file:///home/deploy/pixelnew/.yarn/__virtual__/vite-plugin-ruby-virtual-6c07d208aa/2/.yarn/berry/cache/vite-plugin-ruby-npm-5.1.1-79312225a4-10c0.zip/node_modules/vite-plugin-ruby/dist/index.js";
+import vue from "file:///home/deploy/pixelnew/.yarn/__virtual__/@vitejs-plugin-vue-virtual-f9c5a4d60d/2/.yarn/berry/cache/@vitejs-plugin-vue-npm-5.2.1-25d60c16d1-10c0.zip/node_modules/@vitejs/plugin-vue/dist/index.mjs";
+var vite_config_default = defineConfig({
+  plugins: [
+    RubyPlugin(),
+    vue()
+  ]
+});
+export {
+  vite_config_default as default
+};
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsidml0ZS5jb25maWcubXRzIl0sCiAgInNvdXJjZXNDb250ZW50IjogWyJjb25zdCBfX3ZpdGVfaW5qZWN0ZWRfb3JpZ2luYWxfZGlybmFtZSA9IFwiL2hvbWUvZGVwbG95L3BpeGVsbmV3XCI7Y29uc3QgX192aXRlX2luamVjdGVkX29yaWdpbmFsX2ZpbGVuYW1lID0gXCIvaG9tZS9kZXBsb3kvcGl4ZWxuZXcvdml0ZS5jb25maWcubXRzXCI7Y29uc3QgX192aXRlX2luamVjdGVkX29yaWdpbmFsX2ltcG9ydF9tZXRhX3VybCA9IFwiZmlsZTovLy9ob21lL2RlcGxveS9waXhlbG5ldy92aXRlLmNvbmZpZy5tdHNcIjtpbXBvcnQgeyBkZWZpbmVDb25maWcgfSBmcm9tICd2aXRlJ1xuaW1wb3J0IFJ1YnlQbHVnaW4gZnJvbSAndml0ZS1wbHVnaW4tcnVieSdcbmltcG9ydCB2dWUgZnJvbSAnQHZpdGVqcy9wbHVnaW4tdnVlJ1xuXG5leHBvcnQgZGVmYXVsdCBkZWZpbmVDb25maWcoe1xuICBwbHVnaW5zOiBbXG4gICAgUnVieVBsdWdpbigpLFxuICAgIHZ1ZSgpXG4gIF0sXG59KVxuIl0sCiAgIm1hcHBpbmdzIjogIjtBQUFtUCxTQUFTLG9CQUFvQjtBQUNoUixPQUFPLGdCQUFnQjtBQUN2QixPQUFPLFNBQVM7QUFFaEIsSUFBTyxzQkFBUSxhQUFhO0FBQUEsRUFDMUIsU0FBUztBQUFBLElBQ1AsV0FBVztBQUFBLElBQ1gsSUFBSTtBQUFBLEVBQ047QUFDRixDQUFDOyIsCiAgIm5hbWVzIjogW10KfQo=

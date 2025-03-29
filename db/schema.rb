81ebed8ffafae8dba0c0734809c663436d090091
bananvyhe <loadonden@yahoo.com>
2025-03-29 15:11:52 +0500

    edit user model

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_28_182423) do
   enable_extension "pg_catalog.plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

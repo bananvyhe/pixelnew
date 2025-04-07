c87fac2be438324173ebe63d7367340ea15eb2ec
bananvyhe <loadonden@yahoo.com>
2025-04-07 20:34:05 +0500

    add light/dark themes

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -19,22 +19,65 @@ import { createPinia } from 'pinia'
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
 import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
+import { createVuetify} from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import Notifications from '@kyvg/vue3-notification'
 import VueAxios from 'vue-axios'
-
 const pinia = createPinia()
 const app = createApp(App);
+
+const customDarkTheme = {
+  dark: true,
+  colors: {
+     // background: "#adaada",
+    // background: "#15202b",
+    // surface: "#15202b",
+     
+ 
+    primary: "#3f51b5",
+    secondary: "#03dac6",
+    error: "#ff5722",
+  },
+};
+const customLightTheme = {
+  dark: false,
+  colors: {
+    background: '#FFFFFF',
+    surface: '#FFFFFF',
+    primary: '#6200EE',
+    'primary-darken-1': '#3700B3',
+    secondary: '#03DAC6',
+    'secondary-darken-1': '#018786',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  },
+}
+
 const vuetify = createVuetify({
   components,
   directives,
+  theme: {
+    defaultTheme:  'customLightTheme',
+    themes: {
+      customDarkTheme,
+      customLightTheme,
+    },
+  },
+ 
 })
 const router = createRouter({
   history: createWebHistory(),
   routes,
 })
+
+
+
+
+
+
 app.use(VueAxios, {
   secured: securedAxiosInstance,
   plain: plainAxiosInstance
